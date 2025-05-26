243c67dc01072cb3ee485bfb964b2a3a454f61fb
Abishek Newar <abisheknewarcoolplugins@gmail.com>
2025-05-26 12:53:54 +0530

    changes

--- frontend/lib.js
+++ frontend/lib.js
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://jobportal-lovat.vercel.app/v1"
-export const BACKEND_URL = "http://localhost:3000/v1"
+export const BACKEND_URL = "https://jobportal-lovat.vercel.app/v1"
+// export const BACKEND_URL = "http://localhost:3000/v1"
