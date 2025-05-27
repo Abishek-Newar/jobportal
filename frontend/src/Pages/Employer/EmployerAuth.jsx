6f608b24bd258f741d4b83d289dcbfa0d5efe5c0
Abishek Newar <abisheknewarcoolplugins@gmail.com>
2025-05-27 12:25:56 +0530

    changes

--- frontend/src/Pages/Employer/EmployerAuth.jsx
+++ frontend/src/Pages/Employer/EmployerAuth.jsx
@@ -7,7 +7,7 @@ const EmployerAuth = () => {
     const [authType,setAuthType] = useState("signup")
   return (
     <div className='  flex '>
-        <div className='w-[40vw] min-h-screen bg-center' style={{backgroundImage: `url("https://img.freepik.com/free-photo/working-dark-room_1098-12825.jpg?t=st=1748327996~exp=1748331596~hmac=f11382cb5ac832568dd7916c8b1fb49be5725cc02f12a37ee735f8375f31f608&w=1380")`,backgroundSize:"cover"}}></div>
+        <div className='w-[40vw] min-h-screen bg-center hidden md:block' style={{backgroundImage: `url("https://img.freepik.com/free-photo/working-dark-room_1098-12825.jpg?t=st=1748327996~exp=1748331596~hmac=f11382cb5ac832568dd7916c8b1fb49be5725cc02f12a37ee735f8375f31f608&w=1380")`,backgroundSize:"cover"}}></div>
         <div className='w-[60vw] min-h-screen flex justify-center items-center'>
             {
                 authType === "signup"?
